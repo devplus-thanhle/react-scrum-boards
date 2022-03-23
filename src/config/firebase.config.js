ae4b4f4621523e53e28aec5a0d9aeaceed46787c
ThanhLe <thanhlesy.devplus@gmail.com>
2022-03-23 09:38:38 +0700

    add

---
+++ src/config/firebase.config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyDU4GQfzTgNWM2IWk4RupDTnbELH7fxC_I",
+  authDomain: "react-firebase-24f51.firebaseapp.com",
+  databaseURL: "https://react-firebase-24f51-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  projectId: "react-firebase-24f51",
+  storageBucket: "react-firebase-24f51.appspot.com",
+  messagingSenderId: "429835226596"
+};
